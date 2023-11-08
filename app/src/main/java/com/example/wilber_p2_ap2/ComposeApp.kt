8decfe78816189bb356c4aca2cd7a3f2edcd9dfe
guibe22 <galvezwilber22@gmail.com>
2023-11-08 18:04:19 -0400

    first commit

---
+++ app/src/main/java/com/example/wilber_p2_ap2/ComposeApp.kt
@@ -0,0 +1,8 @@
+package com.example.wilber_p2_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeApp: Application()  {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WIlber_p2_ap2"
+include(":app")
+ 
